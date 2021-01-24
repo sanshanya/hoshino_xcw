0a1ec49009ffdc2ea2e6419709df722760ef6ecc
sanshanya <1057069677@qq.com>
2021-01-24 17:17:12 +0800

    避免yobot与webservies的冲突

--- XCW/hoshino/hoshino/modules/shebot/webServiceManager/view.py
+++ XCW/hoshino/hoshino/modules/shebot/webServiceManager/view.py
@@ -1,8 +1,8 @@
 import nonebot
 import json
-from hoshino import config
+
 from datetime import timedelta
-from hoshino import Service
+from hoshino import Service, priv, config
 from quart import request,session,redirect,Blueprint
 from .data_source import render_template,get_random_str
 
@@ -13,82 +13,102 @@ if not app.config.get('SECRET_KEY'):
     app.config['SECRET_KEY'] = get_random_str(10)
 
 public_address = config.IP #改为你服务器的公网ip,域名应该也可以，我没试过
-port = bot.config.PORT
+port = config.PORT
 passwd = config.PassWord #登录密码
+
+sv_help = '''
+服务开关网页端
+私聊[bot设置]
+'''.strip()
+
+sv = Service(
+    name = '网页服务开关',  #功能名
+    use_priv = priv.SUPERUSER, #使用权限   
+    manage_priv = priv.SUPERUSER, #管理权限
+    visible = False, #False隐藏
+    enable_on_default = True, #是否默认启用
+    bundle = '通用', #属于哪一类
+    help_ = sv_help #帮助文本
+    )
+
+@sv.on_fullmatch(["帮助网页服务开关"])
+async def bangzhu(bot, ev):
+    await bot.send(ev, sv_help, at_sender=True)
 
 @switcher.before_request
 async def _():
     user_ip = request.remote_addr
-    if request.path == '/sv/login':
+    if request.path == '/svlogin':
         return
-    if request.path == '/sv/check':
+    if request.path == '/check':
         return
     if session.get('user_ip') == user_ip:
         return
-    return redirect('/sv/login') 
+    return redirect('/svlogin') 
 
-@switcher.route('/sv/login',methods=['GET','POST'])
-async def svlogin():
+@switcher.route('/svlogin',methods=['GET','POST'])
+async def login():
     print(request.method)
     if request.method == 'GET':
         return await render_template('login.html',passwd=passwd,public_address=public_address,port=port)
     else:
-        svlogin_data = await request.form
-        input_psd = svlogin_data.get('password')
+        login_data = await request.form
+        input_psd = login_data.get('password')
         if input_psd == passwd:
             user_ip = request.remote_addr
             session['user_ip'] = user_ip
             session.permanent = True
             app.permanent_session_lifetime = timedelta(weeks=2)
-            return redirect('/sv/manager')
+            return redirect('/svmanager')
         else:
-            return redirect('/sv/login')
+            return redirect('/svlogin')
 
-@switcher.route('/sv/manager')
-async def manager():
+
+@switcher.route('/svmanager')
+async def svmanager():
     return await render_template('main.html',public_address=public_address,port=port)
 
-@switcher.route('/sv/group')
+@switcher.route('/svgroup')
 async def test():
-    svgroups = await get_svgroups()
-    return await render_template('by_group.html',items=svgroups,public_address=public_address,port=port)
+    groups = await get_groups()
+    return await render_template('by_group.html',items=groups,public_address=public_address,port=port)
 
-@switcher.route('/sv/service')
+@switcher.route('/svservice')
 async def show_all_services():
     svs = Service.get_loaded_services()
     sv_names = list(svs)
     return await render_template('by_service.html',items=sv_names,public_address=public_address,port=port)
 
-@switcher.route('/sv/group/<gid_str>')
-async def show_svgroup_services(gid_str:str):
+@switcher.route('/svgroup/<gid_str>')
+async def show_group_services(gid_str:str):
     gid = int(gid_str)
     svs = Service.get_loaded_services()
     conf = {}
     conf[gid_str] = {}
     for key in svs:
         conf[gid_str][key] = svs[key].check_enabled(gid)
-    return await render_template('group_services.html',svgroup_id=gid_str,conf=conf,public_address=public_address,port=port)
+    return await render_template('group_services.html',group_id=gid_str,conf=conf,public_address=public_address,port=port)
 
-@switcher.route('/sv/service/<sv_name>')
-async def show_service_svgroups(sv_name:str):
+@switcher.route('/svservice/<sv_name>')
+async def show_service_groups(sv_name:str):
     svs = Service.get_loaded_services()
-    svgroups = await get_svgroups()
+    groups = await get_groups()
     conf = {}
-    for svgroup in svgroups :
-        gid = svgroup['svgroup_id']
+    for group in groups :
+        gid = group['group_id']
         gid_str = str(gid)
         conf[gid_str] = {}
         if svs[sv_name].check_enabled(gid):
             conf[gid_str][sv_name] = True
         else:
             conf[gid_str][sv_name] = False
-    return await render_template('service_groups.html',sv_name=sv_name,conf=conf,svgroups=svgroups,public_address=public_address,port=port)
+    return await render_template('service_groups.html',sv_name=sv_name,conf=conf,groups=groups,public_address=public_address,port=port)
 
-async def get_svgroups():
-    return await bot.get_svgroup_list()
+async def get_groups():
+    return await bot.get_group_list()
 
-@switcher.route('/sv/set/',methods=['GET','POST'])
-async def set_svgroup():
+@switcher.route('/svset/',methods=['GET','POST'])
+async def set_group():
     #接收前端传来的配置数据，数据格式{"<gid>":{'serviceA':True,'serviceB':False}}
     if request.method == 'POST':
         data = await request.get_data()
@@ -110,4 +130,4 @@ async def set_svgroup():
 async def setting(ctx):
     message = ctx['raw_message']
     if message in ['服务管理', 'bot设置']:
-        await bot.send(ctx,f'http://{public_address}:{port}/sv/manager',at_sender=False)
+        await bot.send(ctx,f'http://{public_address}:{port}/svmanager',at_sender=False)
