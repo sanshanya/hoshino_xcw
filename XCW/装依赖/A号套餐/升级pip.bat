f2a0ab66a43f30d0a5e3212e1480c1087f356f4e
sanshanya <1057069677@qq.com>
2021-01-26 15:17:39 +0800

    删库跑路

--- XCW/装依赖/A号套餐/升级pip.bat
+++
@@ -1,2 +0,0 @@
-py -3.8 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
-exit
--- XCW/装依赖/A号套餐/装依赖.bat
+++
@@ -1,2 +0,0 @@
-start py -3.8 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
-exit
